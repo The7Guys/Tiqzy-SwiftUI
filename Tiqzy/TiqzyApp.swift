ced23a8f4d64d3bfc60af9c57dfa63f54c66df9a
AndreiSchereper <andrei.schereper@gmail.com>
2024-12-10 02:14:43 +0100

    Initial Commit

---
+++ Tiqzy/TiqzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiqzyApp.swift
+//  Tiqzy
+//
+//  Created by Andrei on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TiqzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
